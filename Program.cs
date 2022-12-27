daaae08a6e9be9eeda7af6365489dd0b050126eb
Shamil Celilzade <shamillcelilzade@gmail.com>
2022-12-27 09:51:43 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using MathLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Class1.Toplama(5,15));
+
+            Console.ReadLine();
+        }
+    }
+}
